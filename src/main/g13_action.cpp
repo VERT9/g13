cace82675e8f59635322e054da11c3bced9b4314
VERT9 <vert9x@gmail.com>
2025-01-15 21:38:37 -0600

    Removed circular referencing to G13_Device in G13_Action Updated references to G13_Log and G13_Keymap in G13_Action

--- src/main/g13_action.cpp
+++ src/main/g13_action.cpp
@@ -2,31 +2,22 @@
 // Created by vert9 on 11/23/23.
 //
 
-#include "helper.h"
+#include <utility>
+
+#include "g13.h"
 #include "g13_log.h"
 #include "g13_device.h"
 #include "g13_action.h"
-
-#include <utility>
+#include "helper.h"
 
 using Helper::repr;
 
 namespace G13 {
-	inline G13_Manager& G13_Action::manager() {
-		return _keypad.manager();
-	}
-
-	inline const G13_Manager& G13_Action::manager() const {
-		return _keypad.manager();
-	}
-
-	G13_Action::~G13_Action() {}
-
-	G13_Action_Keys::G13_Action_Keys(G13_Device& keypad, G13_Log& logger, const std::string& keys_string) : G13_Action(keypad, logger) {
-		std::stringstream buffer(keys_string);
+	G13_Action_Keys::G13_Action_Keys(std::shared_ptr<G13_Log> logger, std::shared_ptr<G13_KeyMap> keymap, std::string keys) : G13_Action(std::move(logger), std::move(keymap)) {
+		std::stringstream buffer(keys);
 		std::string key;
 		while (getline(buffer, key, '+')) {
-			auto kval = manager().find_input_key_value(key);
+			auto kval = _keymap->find_input_key_value(key);
 			if (kval == BAD_KEY_VALUE) {
 				throw G13_CommandException("create action unknown key : " + key);
 			}
@@ -34,15 +25,13 @@ namespace G13 {
 		}
 	}
 
-	G13_Action_Keys::~G13_Action_Keys() {}
-
-	void G13_Action_Keys::act(G13_Device& g13, bool is_down) {
+	void G13_Action_Keys::act(const bool is_down, G13_Device& device) {
 		for (int _key : _keys) {
-			g13.send_event(EV_KEY, _key, is_down);
+			device.send_event(EV_KEY, _key, is_down);
 			if (is_down) {
-				_logger.trace("sending KEY DOWN " + std::to_string(_key));
+				_logger->trace("sending KEY DOWN " + std::to_string(_key));
 			} else {
-				_logger.trace("sending KEY UP " + std::to_string(_key));
+				_logger->trace("sending KEY UP " + std::to_string(_key));
 			}
 		}
 	}
@@ -52,18 +41,16 @@ namespace G13 {
 
 		for (size_t i = 0; i < _keys.size(); i++) {
 			if (i) out << " + ";
-			out << manager().find_input_key_name(_keys[i]);
+			out << _keymap->find_input_key_name(_keys[i]);
 		}
 	}
 
-	G13_Action_PipeOut::G13_Action_PipeOut(G13_Device& keypad, G13_Log& logger, const std::string& out) :
-			G13_Action(keypad, logger), _out(out + "\n") {}
+	G13_Action_PipeOut::G13_Action_PipeOut(std::shared_ptr<G13_Log> logger, std::shared_ptr<G13_KeyMap> keymap, std::string out) :
+			G13_Action(std::move(logger), std::move(keymap)), _out(out + "\n") {}
 
-	G13_Action_PipeOut::~G13_Action_PipeOut() {}
-
-	void G13_Action_PipeOut::act(G13_Device& kp, bool is_down) {
+	void G13_Action_PipeOut::act(const bool is_down, G13_Device& device) {
 		if (is_down) {
-			kp.write_output_pipe(_out);
+			device.write_output_pipe(_out);
 		}
 	}
 
@@ -71,14 +58,12 @@ namespace G13 {
 		o << "WRITE PIPE : " << repr(_out);
 	}
 
-	G13_Action_Command::G13_Action_Command(G13_Device& keypad, G13_Log& logger, const std::string& cmd) :
-			G13_Action(keypad, logger), _cmd(cmd) {}
+	G13_Action_Command::G13_Action_Command(std::shared_ptr<G13_Log> logger, std::shared_ptr<G13_KeyMap> keymap, std::string cmd) :
+			G13_Action(std::move(logger), std::move(keymap)), _cmd(std::move(cmd)) {}
 
-	G13_Action_Command::~G13_Action_Command() {}
-
-	void G13_Action_Command::act(G13_Device& kp, bool is_down) {
+	void G13_Action_Command::act(const bool is_down, G13_Device& device) {
 		if (is_down) {
-			keypad().command(_cmd.c_str());
+			device.command(_cmd.c_str());
 		}
 	}
 
@@ -86,20 +71,24 @@ namespace G13 {
 		o << "COMMAND : " << repr(_cmd);
 	}
 
-	void G13_Action_AppChange::act(G13_Device& kp, const bool is_down) {
+	void G13_Action_AppChange::act(const bool is_down, G13_Device& device) {
 		if (is_down) {
-			kp.next_app();
+			device.next_app();
 		}
+
+		_current_app = device.get_current_app();
 	}
 
 	void G13_Action_AppChange::dump(std::ostream& o) const {
-		o << "APP INDEX : " << keypad().get_current_app();
+		o << "APP INDEX : " << _current_app;
 	}
 
-	G13_Action_Dynamic::G13_Action_Dynamic(G13_Device& keypad, G13_Log& logger, std::function<void()> action) :
-			G13_Action(keypad, logger), _action(std::move(action)) {}
+	G13_Action_Dynamic::G13_Action_Dynamic(std::shared_ptr<G13_Log> logger,
+											std::shared_ptr<G13_KeyMap> keymap, std::function<void()> action) :
+		G13_Action(std::move(logger), std::move(keymap)), _action(std::move(action)) {
+	}
 
-	void G13_Action_Dynamic::act(G13_Device& kp, const bool is_down) {
+	void G13_Action_Dynamic::act(const bool is_down, G13_Device& device) {
 		if (is_down) {
 			_action();
 		}
