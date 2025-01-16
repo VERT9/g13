0a7252c1d08a0e99a49d166c6e57e8982f5628aa
VERT9 <vert9x@gmail.com>
2025-01-15 21:57:25 -0600

    Updated the Container to allow for passing in arguments to the Generator functions Organized includes

--- src/main/container.h
+++ src/main/container.h
@@ -5,12 +5,12 @@
 #ifndef CONTAINER_H
 #define CONTAINER_H
 
-#include <unordered_map>
-#include <typeindex>
 #include <any>
 #include <format>
 #include <functional>
 #include <memory>
+#include <typeindex>
+#include <unordered_map>
 
 /**
  * @brief A Simple Inversion of Control (IoC) Container implementation
@@ -23,17 +23,20 @@ class Container {
 			factoryMap.clear();
 		};
 
+		// TODO how to make this argument list dynamic?!
 		template <typename T>
-		using Generator = std::function<std::shared_ptr<T>()>;
+		using Generator = std::function<std::shared_ptr<T>(std::any& arg1, std::any& arg2, std::any& arg3)>;
 
 		template <typename T>
 		void RegisterFactory(Generator<T> gen) {
 			factoryMap[typeid(T)] = gen;
 		}
+
+		// TODO how to make this argument list dynamic?!
 		template <class T>
-		std::shared_ptr<T> Resolve() {
+		std::shared_ptr<T> Resolve(std::any arg1 = nullptr, std::any arg2 = nullptr, std::any arg3 = nullptr) {
 			if (const auto i = factoryMap.find(typeid(T)); i != factoryMap.end()) {
-				return std::any_cast<Generator<T>>(i->second)();
+				return std::any_cast<Generator<T>>(i->second)(arg1, arg2, arg3);
 			}
 
 			throw std::runtime_error(std::format("Could not resolve factory for type [{}]", typeid(T).name()));
