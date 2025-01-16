f993628c426baf3e107819077ca52d27e84f8c83
VERT9 <vert9x@gmail.com>
2025-01-15 21:56:24 -0600

    Removed dependency on Boost in G13_Manager Removed previously moved members and methods from G13_Manager

--- src/main/g13_manager.cpp
+++ src/main/g13_manager.cpp
@@ -2,31 +2,30 @@
 // Created by vert9 on 11/23/23.
 //
 
-#include "helper.h"
-#include <boost/lexical_cast.hpp>
-#include <boost/preprocessor/seq.hpp>
 #include <csignal>
-#include <format>
-#include <wordexp.h>
 #include <filesystem>
+#include <format>
+#include <iostream>
 #include <set>
+#include <sstream>
 
-#include <linux/input.h>
 #include <pugixml.hpp>
+#include <wordexp.h>
 #include <utility>
 
-#include "g13_manager.h"
 #include "g13_device.h"
 #include "g13_keys.h"
+#include "g13_manager.h"
+#include "g13_profile.h"
+#include "g13_stick.h"
+#include "helper.h"
 
 using namespace std;
 using Helper::repr;
 using Helper::find_or_throw;
 
 namespace G13 {
-	#define CONTROL_DIR std::string("/tmp/")
-
-	G13_Manager::G13_Manager(std::shared_ptr<G13_Log> logger) : devs(0), ctx(0), _logger(std::move(logger)) {}
+	G13_Manager::G13_Manager(std::shared_ptr<G13_Log> logger, std::shared_ptr<G13_KeyMap> keymap) : devs(0), ctx(0), _logger(std::move(logger)), _keymap(std::move(keymap)) {}
 
 	bool G13_Manager::running = true;
 
@@ -58,7 +57,10 @@ namespace G13 {
 					_logger->error("Cannot Claim Interface");
 					return;
 				}
-				g13s.push_back(new G13_Device(*this, *_logger, handle, g13s.size()));
+
+				auto device = new G13_Device(_logger, handle, g13s.size());
+				g13s.push_back(device);
+				g13s.back()->init();
 			}
 		}
 	}
@@ -86,35 +88,7 @@ namespace G13 {
 		_string_config_values[name] = value;
 	}
 
-	std::string G13_Manager::make_pipe_name(G13_Device* d, bool is_input) {
-		if (is_input) {
-			std::string config_base = string_config_value("pipe_in");
-			if (config_base.size()) {
-				if (d->id_within_manager() == 0) {
-					return config_base;
-				} else {
-					return config_base + "-" + boost::lexical_cast<std::string>(d->id_within_manager());
-				}
-			}
-
-			return CONTROL_DIR + "g13-" + boost::lexical_cast<std::string>(d->id_within_manager());
-		} else {
-			std::string config_base = string_config_value("pipe_out");
-			if (config_base.size()) {
-				if (d->id_within_manager() == 0) {
-					return config_base;
-				} else {
-					return config_base + "-" + boost::lexical_cast<std::string>(d->id_within_manager());
-				}
-			}
-
-			return CONTROL_DIR + "g13-" + boost::lexical_cast<std::string>(d->id_within_manager()) + "_out";
-		}
-	}
-
 	int G13_Manager::run() {
-
-		init_keynames();
 		display_keys();
 
 		ssize_t cnt;
@@ -136,12 +110,12 @@ namespace G13 {
 		discover_g13s(devs, cnt, g13s);
 		libusb_free_device_list(devs, 1);
 		_logger->info("Found " + std::to_string(g13s.size()) + " G13s");
-		if (g13s.size() == 0) {
+		if (g13s.empty()) {
 			return 1;
 		}
 
 		for (int i = 0; i < g13s.size(); i++) {
-			g13s[i]->register_context(ctx);
+			g13s[i]->register_context(ctx, *this);
 		}
 		signal(SIGINT, set_stop);
 		if (g13s.size() > 0 && logo_filename.size()) {
@@ -180,80 +154,17 @@ namespace G13 {
 	}
 
 	void G13_Manager::init_keynames() {
-
-		int key_index = 0;
-
-		// setup maps to let us convert between strings and G13 key names
-		#define ADD_G13_KEY_MAPPING(r, data, elem)                             \
-        {                                                                      \
-            std::string name = BOOST_PP_STRINGIZE(elem);                       \
-            g13_key_to_name[key_index] = name;                                 \
-            g13_name_to_key[name] = key_index;                                 \
-            key_index++;                                                       \
-        }                                                                      \
-
-		BOOST_PP_SEQ_FOR_EACH(ADD_G13_KEY_MAPPING, _, G13_KEY_SEQ)
-
-		// setup maps to let us convert between strings and linux key names
-		#define ADD_KB_KEY_MAPPING(r, data, elem)                              \
-        {                                                                      \
-            std::string name = BOOST_PP_STRINGIZE(elem);                       \
-            int keyval = BOOST_PP_CAT( KEY_, elem );                           \
-            input_key_to_name[keyval] = name;                                  \
-            input_name_to_key[name] = keyval;                                  \
-        }                                                                      \
-
-		BOOST_PP_SEQ_FOR_EACH(ADD_KB_KEY_MAPPING, _, KB_INPUT_KEY_SEQ)
-	}
-
-	LINUX_KEY_VALUE G13_Manager::find_g13_key_value(const std::string& keyname) const {
-		auto i = g13_name_to_key.find(keyname);
-		if (i == g13_name_to_key.end()) {
-			return BAD_KEY_VALUE;
-		}
-		return i->second;
-	}
-
-	LINUX_KEY_VALUE G13_Manager::find_input_key_value(const std::string& keyname) const {
-
-		// if there is a KEY_ prefix, strip it off
-		if (!strncmp(keyname.c_str(), "KEY_", 4)) {
-			return find_input_key_value(keyname.c_str() + 4);
-		}
-
-		auto i = input_name_to_key.find(keyname);
-		if (i == input_name_to_key.end()) {
-			return BAD_KEY_VALUE;
-		}
-		return i->second;
-	}
-
-	std::string G13_Manager::find_input_key_name(LINUX_KEY_VALUE v) const {
-		try {
-			return find_or_throw(input_key_to_name, v);
-		}
-		catch (...) {
-			return "(unknown linux key)";
-		}
-	}
-
-	std::string G13_Manager::find_g13_key_name(G13_KEY_INDEX v) const {
-		try {
-			return find_or_throw(g13_key_to_name, v);
-		}
-		catch (...) {
-			return "(unknown G13 key)";
-		}
 	}
 
 	void G13_Manager::display_keys() {
+		_logger->info("Known keys on G13:");
+		std::stringstream stream;
+		stream << _keymap->map_G13_keys();
+		_logger->info(stream.str());
 
-		typedef std::map<std::string, int> mapType;
-		G13_OUT("Known keys on G13:");
-		G13_OUT(Helper::map_keys_out(g13_name_to_key));
-
-		G13_OUT("Known keys to map to:");
-		G13_OUT(Helper::map_keys_out(input_name_to_key));
+		_logger->info("Known keys to map to:");
+		stream << _keymap->map_input_keys();
+		_logger->info(stream.str());
 	}
 
 	void G13_Manager::load_profile(G13_Device* device, const std::string& filename) {
